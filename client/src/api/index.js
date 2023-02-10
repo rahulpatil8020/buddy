ef98baef19425f544c63ae1207ccdaad508a8468
rahulpatil8020 <rahuldpatil20@gmail.com>
2023-02-09 17:02:40 -0800

    Added api calls for adventures posts

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -4,3 +4,12 @@ const API = axios.create({ baseURL: "http://localhost:8000" });
 
 export const signup = (formData) => API.post("users/signup", formData);
 export const login = (formData) => API.post("users/login", formData);
+
+export const getAllAdventurePosts = () => API.get("adventures/");
+export const getAdventurePost = (id) => API.get(`adventures/${id}`);
+export const createAdventurePost = (formData) =>
+  API.post("adventures/", formData);
+export const updateAdventurePost = (id, formData) =>
+  API.patch(`adventures/${id}`, formData);
+export const deleteAdventurePost = (id) => API.delete(`adventures/${id}`);
+export const likeAdventurePost = (id) => API.patch(`adventures/${id}/like`);
